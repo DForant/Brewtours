b1b782d36e99a70cee1d653cce6520b3a563dd77
deano252 <dforant@gmail.com>
2023-02-16 14:15:20 -0500

    adds

---
+++ placeholder.php
@@ -0,0 +1,76 @@
+<html lang="en">
+    <head>
+        <title>Brewtours - Placeholder page</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://fonts.googleapis.com/css?family=Amiri:400,700&display=swap" rel="stylesheet">
+        <link href="css/iconfont.css" rel="stylesheet" type="text/css"/>
+        <link href="css/style.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        
+        <?php include 'templates/navigation.php'; ?>
+                
+        <header class="header header--subpage header--subpage-placeholder">
+            <div class="header__text-box">
+                <h1 class="heading-primary">
+                    <span class="heading-primary--sub">A Placeholder page</span>
+                </h1>
+            </div>
+        </header>
+        
+        <main>
+            <section class="subpage-section">
+                <div class="row about-section-item">
+                    <h3 class="heading-tertiary center-text margin-bottom-small">
+                        Brewtours - A simple webpage that can be used for a touring type bussiness
+                    </h3>
+                    <div class="about-section-item__text">
+                        <p>
+                            Brewtours is an example website designed by Dean Forant that can be used for any type of business that focuses on guided tours and excursions. Although the theme is based on Breweries and pubs, the overall theme can be changed to accommodate practically any type of business that sells a product.
+                        </p>
+                    </div>
+                </div>
+
+                <div class="row about-section-item">
+                    <h3 class="heading-tertiary center-text margin-bottom-small">
+                        Your free to use this design but give credit where credit is due.
+                    </h3>
+                    <div class="about-section-item__text">
+                        <p>
+                            You're free to download and use this design for your own use, but I would ask that you not only include a shoutout to <a href="http://deanforant.com">Dean Forant</a> but also <a href="https://www.udemy.com/share/101WkwB0sed1hSTH4=/">Jonas Scmedtmann</a> whose concepts and methods taught In his Udemy HTML course were the basis for the final design. 
+                        </p>
+                        
+                        <p>
+                           The website download includes all the HTML code, SASS code and both the uncompressed CSS and compressed and minified CSS. I also included the .psd file for the logo so that you can customize it in Photoshop, Gimp or your favorite image editing program.  
+                        </p>
+                        
+                        <div class="placeholder placeholder__btn-box">
+                            <a href="downloads/brewtours_1-0-0.zip" class="btn btn--brown">Download Page Design</a>
+                        </div>
+                        
+                    </div>
+                </div>
+
+                <div class="row about-section-item">
+                    <h3 class="heading-tertiary center-text margin-bottom-small">
+                        Are you in need of a professional Web Developer
+                    </h3>
+                    <div class="about-section-item__text">
+                        <p>
+                            Dean Forant is a web Developer and designer with over 15 years' experience in website and applications development. If you are in need of an experienced web Developer, Dean can handle your website requirements no matter how big or small. He has experience in a wide array of web development languages and methodologies that all can be used to create websites that are both modern and responsive across multiple browser platforms and mobile devices.  
+                        </p>
+                        
+                        <div class="placeholder placeholder__btn-box">
+                            <a href="mailto:dean@deanforant.com" class="btn btn--brown">Contact Dean</a>
+                        </div>                        
+                    </div>
+                </div>
+            </section>
+        </main>
+
+        <?php include 'templates/footer.php'; ?>
+    
+    </body>
+    <script> </script>
+</html>
---
+++ templates/footer.php
@@ -0,0 +1,36 @@
+        <footer class="footer">
+            <div class="footer__logo-box">
+                
+                <picture class="footer__logo">
+                    <a href="index.php">
+                        <img srcset="img/logo-1x.gif 450w, img/logo-2x.gif 900w"
+                            sizes="(max-width: 900px) 20vw, (max-width: 600px) 30vw, 300px"
+                            src="img/logo-1x.gif" 
+                            alt="Brewtours Logo" 
+                            class="footer__logo">
+                    </a>
+                </picture>
+            </div>
+            <div class="row">
+                <div class="col-1-2">
+                    <div class="footer__navigation">
+                        <ul class="footer__list">
+                            <li class="footer__item"><a href="placeholder.php" class="footer__link">Company</a></li>
+                            <li class="footer__item"><a href="placeholder.php" class="footer__link">Contact Us</a></li>
+                            <li class="footer__item"><a href="placeholder.php" class="footer__link">Careers</a></li>
+                            <li class="footer__item"><a href="placeholder.php" class="footer__link">Privacy</a></li>
+                            <li class="footer__item"><a href="placeholder.php" class="footer__link">Terms</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-1-2">
+                    <p class="footer__copyright">
+                        Built by <a href="http://deanforant.com" class="footer__link footer__link--small">Dean Forant</a> &copy; 2020.
+                        <br>
+                        Design based off content and techniques taught in Jonas Schmedtmann's Udemy Course 
+                        <a href="https://www.udemy.com/share/101WkwB0sed1hSTH4=/" class="footer__link footer__link--small">Advanced CSS and Sass</a> 
+                        &copy; by Jonas Scmedtmann 
+                    </p>
+                </div>
+            </div>
+        </footer>
